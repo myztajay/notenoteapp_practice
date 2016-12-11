f6e9b68e7807ff2527705b782ad0115a89d69289
myztajay <myztajay@hotmail.com>
2016-12-11 04:25:44 +0000

    added alt login for users in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  root 'welcome#index'
-  
   resources :notes
+  
+  # Creates an alternate root via Devise so that loggin in ppl can have a different root.
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root"
+  end
+  
+  root 'welcome#index'
 
 end
